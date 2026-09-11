de12fbac9dbe5fac38df003620699119139a41b6
Ankesh Rathore <itsrathoresaab@gmail.com>
2026-09-11 15:25:27 +0530

    Initial Commit

---
+++ SnackAndLadders/SnackAndLaddersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnackAndLaddersApp.swift
+//  SnackAndLadders
+//
+//  Created by mac on 11/09/26.
+//
+
+import SwiftUI
+
+@main
+struct SnackAndLaddersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
